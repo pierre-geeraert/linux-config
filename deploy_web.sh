e8ecf45e276c8987f689e6fa1c02c3ce99e79606
pierre-geeraert <pierre.geeraert@viacesi.fr>
2017-05-20 07:59:41 +0200

    add deploy_werb

---
+++ deploy_web.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo -s
+
+#begin pushbullet
+
+apt-get install python-pip
+pip install pushbullet.py
+pb set-key
+
+#pushbullet end
+
+
+#begin web server
+
+apt-get update
+pb push "end update"
+apt-get install apache2 -y
+pb push "end apache"
+apt-get install php5 libapache2-mod-php5 -y
+pb push "end php"
+cd /var/www/html/
+echo "<?php echo "hello world"; ?>" >> index.php
+rm index.html
+pb push "end conf php"
+service apache2 restart
+apt-get install php5-pgsql
+pb push "php psql end"
+#end web server
